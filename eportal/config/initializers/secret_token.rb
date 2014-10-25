cdcc0c423f1e4a6c6a44c1e3e81cc1f44d0cf1c6
bluesky504 <719048757@qq.com>
2014-10-25 00:44:34 -0400

    create master

---
+++ eportal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eportal::Application.config.secret_token = '68a1e609da603ddde3c1f586cc8f896cbc70e3de76069eec9f3fb080bcba3f1fdd36d5236ea1a26119b9565ddc841742c70668e93f3428f9bc69915e679691f8'
